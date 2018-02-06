049f16eae65d40eccf398460f7414c47ab89a790
chairmanmikec <michaelc1012@gmail.com>
2018-02-06 16:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function deepestChild(){
   let lis = document.getElementById('grand-node').querySelectorAll('div')
   let test
   for (let i = 0; i<lis.length; i++){
-    test=lis[i].innerHTML
+    test=lis[i].querySelector('div')
   }return test
 }
