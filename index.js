cb6e64f2c29c17e024a0f3828e5937c676c6d986
chairmanmikec <michaelc1012@gmail.com>
2018-02-06 16:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ function increaseRankBy(n){
   }
 }
 
-function deepestChild(){
-  let lis = document.getElementById('grand-node').querySelectorAll('div')
-  let test
-  for (let i = 0; i<lis.length-1; i++){
-    test=lis[i].querySelector('div')
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
   }
-  return test
+  return node
 }
