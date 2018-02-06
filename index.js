3c1154734488679155a4da7263ceca177b7bd215
chairmanmikec <michaelc1012@gmail.com>
2018-02-06 16:17:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function nestedTarget(){
   return document.getElementById('nested').querySelector('.target')
 }
 
+function increaseRankBy(n){
+  const lis= document.getElementById('app').querySelectorAll('.ul')
+}
