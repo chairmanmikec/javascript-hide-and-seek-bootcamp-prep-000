c4801070b873499c7f980b608404afa6cfa58cbd
chairmanmikec <michaelc1012@gmail.com>
2018-02-06 16:53:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ function increaseRankBy(n){
 }
 
 function deepestChild() {
-  let node = document.getElementById('grand-node')
-
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
-  }
+ let node = document.getElementById('grand-node')
+ let nextNode = node.children[0]
+ 
+ while(nextNode){
+   node = nextNode
+   nextNode = node.children[0]
+ }
   return node
 }
